76e342549c8e0e4da914683d2b0fb76d8ee7dfb1
Gilad <Gilad181920@gmail.com>
2021-08-18 20:06:03 +0300

    WeatherApp

---
+++ main.js
@@ -0,0 +1,55 @@
+const api = {
+    key: "bcd696b99e40703a79b644515f95e72d",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress' , setQuery);
+
+function setQuery(evt){
+    if (evt.keyCode == 13){
+        getResults(searchbox.value)
+        // console.log(searchbox.value);
+    }
+}
+
+function getResults(query){
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayWeather);
+}
+
+function displayWeather(weather){
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerText = `${Math.round(weather.main.temp)}°c`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`
+}
+
+function dateBuilder(d){
+    let months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+    "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
